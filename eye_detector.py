a0aa6380f5de2979454315a640a96e6fe5b5dd1d
anirudra <anirudra@umich.edu>
2021-05-27 17:14:35 +0530

    Added webcam

--- eye_detector.py
+++ eye_detector.py
@@ -1,13 +1,14 @@
 import cv2
 
-def detector():
-    face_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+def detector(img):
 
-    img = cv2.imread("pic.jpg")
+    face_cascade = cv2.CascadeClassifier("prediction/haarcascade_eye.xml")
+
+    #img = cv2.imread("img")
 
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=5, minNeighbors=1)
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=5, minNeighbors=3)
 
     count = 0
     ret = "Eyes open"
@@ -23,7 +24,11 @@ def detector():
         #cv2.putText(img, ret, (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
 
     cv2.imshow("Gray", img)
-
-    cv2.waitKey()
-
     return ret
+
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, img = cap.read()
+    print(detector(img))
+    if cv2.waitKey(1) & 0xFF == ord('q'): # escape when q is pressed
+        break
