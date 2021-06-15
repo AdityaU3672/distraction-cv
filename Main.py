a99e73fa2fe69c92542e4df567ec63d3aa244aaf
Aditya Uchil <75536724+AdityaU3672@users.noreply.github.com>
2021-06-15 22:21:38 +0530

    Added functions for outputting video feed

--- Main.py
+++ Main.py
@@ -10,6 +10,17 @@ def home():
 
     return render_template("index.html")
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__=='__main__':
 
     detector = dlib.get_frontal_face_detector()
